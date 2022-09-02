86821260673e4d00d23d54ee184ee4a95783345a
Marcelo Franchini <marcelo_franchini@hotmail.com>
2022-09-01 21:05:01 -0300

    env

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./App.css";
 import MapPage from "./pages/MapPage";
 
-export const REACT_APP_GOOGLE_API_KEY = "AIzaSyC1x1dlHD4Z21LpQS0cWSYJVgEDODpboWE";
+
+export const REACT_APP_GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY as string
 
 function App() {
   return <MapPage />;
